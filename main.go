05906a9ed928f2336bb4e4118ef17cd8c8bf665d
hzwedt@gmail.com <hzwedt@gmail.com>
2023-06-08 16:25:28 +0100

    mend with io

--- main.go
+++ main.go
@@ -13,39 +13,44 @@ Formula: (num - num(a)) % 26
 package main
 
 import (
-    "fmt"
-    "strings"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 )
 
 type NewWord struct {
-    Words []string
-    Entry []string
+	Words []string
+	Entry []string
 }
 
 func (ptr *NewWord) rotate(num int, entry string) {
-    for _, v := range entry {
-        if v >= 'a' && v <= 'z' {
-            enc := 'a' + ((int(v)-'a')+num)%26
-            ptr.Entry = append(ptr.Entry, string(v))
-            ptr.Words = append(ptr.Words, string(enc))
-        } else {
-            ptr.Entry = append(ptr.Entry, string(v))
-            ptr.Words = append(ptr.Words, string(v))
-        }
-    }
+	for _, v := range entry {
+		if v >= 'a' && v <= 'z' {
+			enc := 'a' + ((int(v)-'a')+num)%26
+			ptr.Entry = append(ptr.Entry, string(v))
+			ptr.Words = append(ptr.Words, string(enc))
+		} else {
+			ptr.Entry = append(ptr.Entry, string(v))
+			ptr.Words = append(ptr.Words, string(v))
+		}
+	}
 }
 
 func main() {
-    ws := NewWord{}
+	ws := NewWord{}
 
-    var num int
-    fmt.Println("Enter rot number: ")
-    fmt.Scanln(&num)
-    var entry string
-    fmt.Println("Enter your string: ")
-    fmt.Scanln(&entry)
-    
-    ws.rotate(num, entry)
-    fmt.Println("Original: ",strings.Join(ws.Entry, ""))
-    fmt.Println("Rotated: ",strings.Join(ws.Words, ""))
+	var num int
+	fmt.Println("Enter rot number: ")
+	fmt.Scanln(&num)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter your string: ")
+	entry, _ := reader.ReadString('\n')
+
+	entry = strings.TrimSuffix(entry, "\n")
+
+	ws.rotate(num, entry)
+	fmt.Println("Original: ", strings.Join(ws.Entry, ""))
+	fmt.Println("Rotated: ", strings.Join(ws.Words, ""))
 }
